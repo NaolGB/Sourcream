4a4f0ec7df49a1b8089765924f870835fb1f8ec9
Naol Basaye <naolgbasaye@gmail.com>
2023-11-21 18:38:08 +0100

    211123 v0.2.0 presentation version

---
+++ table_objects.py
@@ -0,0 +1,9 @@
+# class TAB_VBAK:
+#     def __init__(self, mandt, vbeln) -> None:
+#         self.mandt = mandt
+#         self.vbeln = vbeln
+#         self.ernam = 
+#         self.erdat = 
+#         self.vbtyp = 
+#         self.kunnr = 
+#         self.delco = 
