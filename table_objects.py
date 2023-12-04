6d8935ec1e97a7b57dc214732a3216c2c3efdc4e
Naol Basaye <naolgbasaye@gmail.com>
2023-12-04 15:31:02 +0000

    merge into main

--- table_objects.py
+++
@@ -1,9 +0,0 @@
-# class TAB_VBAK:
-#     def __init__(self, mandt, vbeln) -> None:
-#         self.mandt = mandt
-#         self.vbeln = vbeln
-#         self.ernam = 
-#         self.erdat = 
-#         self.vbtyp = 
-#         self.kunnr = 
-#         self.delco = 
