29139bfc3a79ca03ab7ad83d564b18c3bb471d15
Naol Basaye <naolgbasaye@gmail.com>
2023-12-28 19:38:27 +0000

    partially completed adding OM master data

---
+++ master_data.py
@@ -0,0 +1,197 @@
+import uuid
+from datetime import datetime
+import random
+
+import values, helpers
+
+
+def users(all_users=values.om_users):
+    ADRP_json = {}
+    USR02_json = {}
+    USR21_json = {}
+
+    for k, v in all_users.items():
+        person_number = str(uuid.uuid4())
+        ADRP_json[str(uuid.uuid4())] = {
+            "CLIENT": values.mandt,
+            "DATE_FROM": '00010101',
+            "NAME_FIRST": k, # TODO split bname
+            "NAME_LAST": k, # TODO split bname
+            "NATION": v['nation'],
+            "PERSNUMBER": person_number 
+        }
+        USR02_json[str(uuid.uuid4())] = {
+            "BNAME": k,
+            "MANDT": values.mandt,
+            "USTYP": v['type']
+        }
+        USR21_json[str(uuid.uuid4())] = {
+            "BNAME": k,
+            "MANDT": values.mandt,
+            "PERSNUMBER": person_number
+        }
+    
+    return {'ADRP_json': ADRP_json, 'USR02_json': USR02_json, 'USR21_json': USR21_json}
+
+def customers(all_customers=values.om_customers, all_users=values.om_users):
+    KNB1_json = {}
+    KNA1_json = {}
+    for k, v in all_customers.items():
+        customer_number = str(uuid.uuid4())
+        KNB1_json[str(uuid.uuid4())] = {
+            "BUKRS": v['company_code'],
+            "ERDAT": helpers.generate_random_date(start_date=datetime(2021, 1, 1), end_date=datetime(2022, 1, 1)),
+            "ERNAM": random.choice(list(all_users.keys())),
+            "KUNNR": customer_number,
+            "MANDT": values.mandt,
+            "ZTERM": v['payment_term']
+        }
+        KNA1_json[str(uuid.uuid4())] = {
+            "ERNAM": random.choice(list(all_users.keys())),
+            "KUNNR": customer_number,
+            "LAND1": v['country'],
+            "MANDT": values.mandt,
+            "NAME1": k,
+            "ORT01": v['city'],
+            "PSTLZ": 'PSTC', # TODO add postal code
+            "REGIO": v['region'],
+            "STRAS": 'ADD1', # TODO add address liner
+            "VBUND": None
+        }
+    
+    return {'KNB1_json': KNB1_json, 'KNA1_json': KNA1_json}
+
+def plants(all_plants=values.om_plants):
+    T001W_json = {}
+    T005T_json = {}
+    for k, v in all_plants.items():
+        T001W_json[str(uuid.uuid4())] = {
+            "LAND1": v['country_key'],
+            "MANDT": values.mandt,
+            "NAME1": k,
+            "WERKS": v['plant_number'],
+            "BWKEY": 'D' # TODO add valuation area
+
+        }
+        T005T_json[str(uuid.uuid4())] = {
+            "LAND1": v['country_key'],
+            "LANDX": v['country_name'],
+            "MANDT": values.mandt,
+            "SPRAS": "E"
+        }
+    
+    return {'T001W_json': T001W_json, 'T005T_json': T005T_json}
+
+def materials(all_materials=values.om_materials, all_users=values.om_users):
+    MAKT_json = {}
+    MARA_json = {}
+    MARM_json = {}
+    T006_json = {}
+    T006D_json = {}
+    T023T_json = {}
+    T134T_json = {}
+    T137T_json = {}
+    MARC_json = {}
+    MBEW_json = {}
+
+    for k, v in all_materials.items():
+        matnr = str(uuid.uuid4())
+        quantity = random.randint(500, 1500)
+        creation_time = helpers.generate_random_date(start_date=datetime(2021, 1, 1), end_date=datetime(2022, 1, 1)),
+        MAKT_json[str(uuid.uuid4())] = {
+            "MAKTX": k,
+            "MANDT": values.mandt,
+            "MATNR": matnr,
+            "SPRAS": "E"
+        }
+        MARA_json[str(uuid.uuid4())] = {
+            "ERNAM": random.choice(list(all_users.keys())),
+            "ERSDA": creation_time,
+            "MANDT": values.mandt,
+            "MATKL": 'DEFAULT', # TODO add material group
+            "MATNR": matnr,
+            "MBRSH": 'D', # TODO add industry sector
+            "MEINS": 'UNT', 
+            "MTART": 'DEFT', # TODO add material type here
+            "PRDHA": 'DEFAULT', # TODO add product heirarchy here
+        }
+        MARM_json[str(uuid.uuid4())] = {
+            "MANDT": values.mandt,
+            "MATNR": matnr,
+            "MEINH": 'ST'
+        }
+        T006_json[str(uuid.uuid4())] = {
+            "DIMID": 'AAAADL',
+            "MANDT": values.mandt,
+            "MSEHI": 'UNT'
+        }
+        T006D_json[str(uuid.uuid4())] = {
+            "DIMID": 'DEF', # TODO add dimension 
+            "MANDT": values.mandt,
+            "MSSIE": 'ST'
+        }
+        T023T_json[str(uuid.uuid4())] = {
+            "MANDT": values.mandt,
+            "MATKL": 'DEFAULT', # TODO add material group - must match MARA.MATKL
+            "SPRAS": "E",
+            "WGBEZ": 'DEFAULT', # TODO add group decriptin
+        }
+        T134T_json[str(uuid.uuid4())] = {
+            "MANDT": values.mandt,
+            "MTART": 'DEFT', # TODO add material type here - must match MARA.MTART
+            "SPRAS": "E"
+        }
+        T137T_json[str(uuid.uuid4())] = {
+            "MANDT": values.mandt,
+            "MBRSH": 'D', # TODO add industry sector - must match MARA.MBRSH
+            "SPRAS": "E"
+        }
+        MARC_json[str(uuid.uuid4())] = {
+            "AUSDT": helpers.generate_random_date(start_date=datetime(2024, 1, 1), end_date=datetime(2025, 1, 1)), # HACK after all SO and procurement have passed
+            "BESKZ": v['type'],
+            "BSTMI": 99,
+            "DISGR": 'D', # TODO add custom value
+            "DISMM": 'D', # TODO add custom value
+            "DISPO": 'D', # TODO add custom value
+            "DZEIT": 99, # TODO add custom value
+            "EISBE": 99, # TODO add custom value
+            "LGRAD": 99, # TODO add custom value
+            "MANDT": values.mandt,
+            "MATNR": matnr,
+            "MMSTD": creation_time,
+            "NFMAT": matnr,
+            "PLIFZ": v['delivery_takes_days'],
+            "STRGR": 'D', # TODO add custom value
+            "WEBAZ": v['goods_receipt_processing_days'],
+            "WERKS": '0001', # TODO make this material dependent than just UK-1 (check values.om_plants)
+        }
+        MBEW_json[str(uuid.uuid4())] = {
+            "BWKEY": 'D', # TODO add custom value - must match T100W.BWKEY
+            "BWTAR": None,
+            "LBKUM": quantity,
+            "MANDT": values.mandt,
+            "MATNR": matnr,
+            "PEINH": 99, # TODO add custom value
+            "SALK3": quantity*v['price'],
+            "STPRS": v['price'],
+            "VERPR": v['price'],
+            "VPRSV": 'D', # TODO add custom value
+        }
+
+        "T001"."BUKRS"
+        "T001"."BUTXT"
+        "T001"."MANDT"
+        "T001"."WAERS"
+
+        "T001K"."BUKRS"
+        "T001K"."BWKEY"
+        "T001K"."MANDT"
+    
+    return #{'MAKT_json': MAKT_json,
+            'MARA_json': MARA_json,
+            'MARM_json': MARM_json,
+            'T006_json': T006_json,
+            'T006D_json': T006D_json,
+            'T023T_json': T023T_json,
+            'T134T_json': T134T_json,
+            'T137T_json': T137T_json,}
--- values.py
+++ values.py
@@ -1,5 +1,6 @@
 mandt = 'SC1'
 
+
 om_sales_order_doc_types = ['C', 'I'] # Order, Order w/o charge
 
 om_shipping_conditions = [
@@ -34,13 +35,6 @@ om_inco2_terms = [
     'E',
 ]
 
-om_zterms = [
-    '01',
-    '02',
-    '03',
-    '04',
-    '05',
-]
 
 om_sales_orgs = {
     'A': {
@@ -68,119 +62,123 @@ om_credit_control_areas = [
     '05',
 ]
 
+om_plants = {
+    "UK-1": {'country_key': 'UK', 'country_name': 'United Kingdom', 'plant_number': '0001'}
+}
+
 om_customers = {
-    "Tasty Bites Co.": {"credit_risk": 0.61},
-    "Brewmaster's Blend": {"credit_risk": 0.28},
-    "Crispy Cravings": {"credit_risk": 0.85},
-    "Sips & Savories": {"credit_risk": 0.68},
-    "Gourmet Delights Inc.": {"credit_risk": 0.21},
-    "Flavor Fusion Foods": {"credit_risk": 0.43},
-    "Café Elegance": {"credit_risk": 0.29},
-    "FreshNosh": {"credit_risk": 0.73},
-    "SodaStreamers": {"credit_risk": 0.48},
-    "Sweets & Sips": {"credit_risk": 0.78},
-    "Gastronomy Galore": {"credit_risk": 0.23},
-    "Nectar Nook": {"credit_risk": 0.35},
-    "Spice & Savor": {"credit_risk": 0.54},
-    "Wholesome Treats": {"credit_risk": 0.52},
-    "Munchies Magic": {"credit_risk": 0.71},
-    "Siplicity Drinks": {"credit_risk": 0.78},
-    "Foodie Fantasy": {"credit_risk": 0.37},
-    "Yummy Morsels": {"credit_risk": 0.51},
-    "Baker's Bliss Co.": {"credit_risk": 0.67},
-    "ThirstQuencher": {"credit_risk": 0.13},
-    "Epicurean Eats": {"credit_risk": 0.78},
-    "Gusto Gourmets": {"credit_risk": 0.43},
-    "Beverage Bliss": {"credit_risk": 0.44},
-    "Taste Troupe": {"credit_risk": 0.77},
-    "ChocoCharm Confections": {"credit_risk": 0.23},
-    "SavorStreet": {"credit_risk": 0.40},
-    "Culinary Crafters": {"credit_risk": 0.70},
-    "BrewBurst Beverages": {"credit_risk": 0.09},
-    "NoshNation": {"credit_risk": 0.82},
-    "Gastronomic Gather": {"credit_risk": 0.38},
-    "Greyjoy Foods Inc.": {"credit_risk": 0.63},
+    "Tasty Bites Co.": {"credit_risk": 0.61, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Brewmaster's Blend": {"credit_risk": 0.28, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Crispy Cravings": {"credit_risk": 0.85, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Sips & Savories": {"credit_risk": 0.68, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Gourmet Delights Inc.": {"credit_risk": 0.21, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Flavor Fusion Foods": {"credit_risk": 0.43, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Café Elegance": {"credit_risk": 0.29, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "FreshNosh": {"credit_risk": 0.73, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "SodaStreamers": {"credit_risk": 0.48, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Sweets & Sips": {"credit_risk": 0.78, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Gastronomy Galore": {"credit_risk": 0.23, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Nectar Nook": {"credit_risk": 0.35, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Spice & Savor": {"credit_risk": 0.54, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Wholesome Treats": {"credit_risk": 0.52, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Munchies Magic": {"credit_risk": 0.71, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Siplicity Drinks": {"credit_risk": 0.78, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Foodie Fantasy": {"credit_risk": 0.37, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Yummy Morsels": {"credit_risk": 0.51, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Baker's Bliss Co.": {"credit_risk": 0.67, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "ThirstQuencher": {"credit_risk": 0.13, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Epicurean Eats": {"credit_risk": 0.78, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Gusto Gourmets": {"credit_risk": 0.43, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Beverage Bliss": {"credit_risk": 0.44, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Taste Troupe": {"credit_risk": 0.77, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "ChocoCharm Confections": {"credit_risk": 0.23, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "SavorStreet": {"credit_risk": 0.40, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Culinary Crafters": {"credit_risk": 0.70, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "BrewBurst Beverages": {"credit_risk": 0.09, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "NoshNation": {"credit_risk": 0.82, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Gastronomic Gather": {"credit_risk": 0.38, 'payment_term': 'Z030', 'country': 'UK', 'region': 'EMEA', 'city': 'LDN', 'company_code': '0001'},
+    "Greyjoy Foods Inc.": {"credit_risk": 0.63, 'payment_term': 'Z030', 'country': 'Westeros', 'region': 'Iron Islands', 'city': 'Pyke', 'company_code': '0001'},
 }
 
 
 om_users = {
-    "Alex Johnson": 'A',
-    "Bailey Smith": 'A',
-    "Carmen Davis": 'A',
-    "Darnell Williams": 'A',
-    "Evelyn Brown": 'A',
-    "Francis Taylor": 'A',
-    "Gabriel Moore": 'A',
-    "Harper Clark": 'A',
-    "Ibrahim Lewis": 'A',
-    "Jaden Hall": 'A',
-    "Kai Lee": 'A',
-    "Logan Mitchell": 'A',
-    "Morgan Anderson": 'A',
-    "Nathan Harris": 'A',
-    "Olive White": 'A',
-    "Peyton Turner": 'A',
-    "Quinn Martin": 'A',
-    "Riley Walker": 'A',
-    "Sasha Baker": 'A',
-    "Taylor Turner": 'A',
-    'Catelyn Stark': 'A',
-    "BATCH_JOB": 'B',
+    "Alex Johnson": {'type': 'A', 'nation': 'UK'},
+    "Bailey Smith": {'type': 'A', 'nation': 'UK'},
+    "Carmen Davis": {'type': 'A', 'nation': 'UK'},
+    "Darnell Williams": {'type': 'A', 'nation': 'UK'},
+    "Evelyn Brown": {'type': 'A', 'nation': 'UK'},
+    "Francis Taylor": {'type': 'A', 'nation': 'UK'},
+    "Gabriel Moore": {'type': 'A', 'nation': 'UK'},
+    "Harper Clark": {'type': 'A', 'nation': 'UK'},
+    "Ibrahim Lewis": {'type': 'A', 'nation': 'UK'},
+    "Jaden Hall": {'type': 'A', 'nation': 'UK'},
+    "Kai Lee": {'type': 'A', 'nation': 'UK'},
+    "Logan Mitchell": {'type': 'A', 'nation': 'UK'},
+    "Morgan Anderson": {'type': 'A', 'nation': 'UK'},
+    "Nathan Harris": {'type': 'A', 'nation': 'UK'},
+    "Olive White": {'type': 'A', 'nation': 'UK'},
+    "Peyton Turner": {'type': 'A', 'nation': 'UK'},
+    "Quinn Martin": {'type': 'A', 'nation': 'UK'},
+    "Riley Walker": {'type': 'A', 'nation': 'UK'},
+    "Sasha Baker": {'type': 'A', 'nation': 'UK'},
+    "Taylor Turner": {'type': 'A', 'nation': 'UK'},
+    'Catelyn Stark': {'type': 'A', 'nation': 'Winter Fell'},
+    "BATCH_JOB": {'type': 'B', 'nation': 'UK'},
 }
 
 om_materials = {
-    "Rice": {"price": 2.99, "availability": 0.8},
-    "Coffee": {"price": 5.49, "availability": 0.6},
-    "Tea": {"price": 3.99, "availability": 0.7},
-    "Soda": {"price": 1.49, "availability": 0.9},
-    "Bottled Water": {"price": 0.99, "availability": 1.0},
-    "Chips": {"price": 2.79, "availability": 0.8},
-    "Cookies": {"price": 3.49, "availability": 0.6},
-    "Candy": {"price": 1.99, "availability": 0.7},
-    "Ice Cream": {"price": 4.99, "availability": 0.5},
-    "Frozen Pizza": {"price": 5.99, "availability": 0.5},
-    "Canned Soup": {"price": 1.79, "availability": 0.9},
-    "Honey": {"price": 4.29, "availability": 0.8},
-    "Peanut Butter": {"price": 3.29, "availability": 0.7},
-    "Jam": {"price": 2.99, "availability": 0.8},
-    "Canned Vegetables": {"price": 1.69, "availability": 0.9},
-    "Frozen Vegetables": {"price": 2.49, "availability": 0.8},
-    "Olive Oil": {"price": 6.99, "availability": 0.6},
-    "Vinegar": {"price": 2.19, "availability": 0.7},
-    "Balsamic Vinegar": {"price": 4.99, "availability": 0.6},
-    "Wine": {"price": 9.99, "availability": 0.4},
-    "Beer": {"price": 1.99, "availability": 0.7},
-    "Soy Sauce": {"price": 2.29, "availability": 0.8},
-    "Worcestershire Sauce": {"price": 2.79, "availability": 0.6},
-    "Pickles": {"price": 2.49, "availability": 0.8},
-    "Olives": {"price": 3.29, "availability": 0.6},
-    "Canned Tuna": {"price": 1.99, "availability": 0.7},
-    "Canned Salmon": {"price": 3.99, "availability": 0.5},
-    "Dried Fruits": {"price": 4.49, "availability": 0.6},
-    "Nuts": {"price": 5.99, "availability": 0.5},
-    "Hamburger Buns": {"price": 2.49, "availability": 0.9},
-    "Hot Dog Buns": {"price": 2.29, "availability": 0.7},
-    "Mustard": {"price": 1.79, "availability": 0.9},
-    "Barbecue Sauce": {"price": 2.99, "availability": 0.8},
-    "Salsa": {"price": 2.49, "availability": 0.7},
-    "Tortilla Chips": {"price": 2.79, "availability": 0.8},
-    "Sour Cream": {"price": 1.99, "availability": 0.9},
-    "Cottage Cheese": {"price": 3.29, "availability": 0.6},
-    "Pudding": {"price": 1.49, "availability": 0.95},
-    "Cereal Bars": {"price": 3.49, "availability": 0.6},
-    "Oatmeal": {"price": 2.99, "availability": 0.7},
-    "Frozen Burritos": {"price": 4.49, "availability": 0.5},
-    "Frozen Dinners": {"price": 3.99, "availability": 0.6},
-    "Sausages": {"price": 5.49, "availability": 0.5},
-    "Muffins": {"price": 2.79, "availability": 0.7},
-    "Bagels": {"price": 2.49, "availability": 0.9},
-    "Donuts": {"price": 1.99, "availability": 0.95},
-    "Canned Fruit": {"price": 2.29, "availability": 0.9},
-    "Fruit Cups": {"price": 2.99, "availability": 0.7},
-    "Almond Milk": {"price": 3.49, "availability": 0.8},
-    "Coconut Water": {"price": 2.79, "availability": 0.7},
-    "Trail Mix": {"price": 4.9, "availability": 0.1},
-    "Lemon Cake": {"price": 3.9, "availability": 0.6},
+    "Rice": {"price": 2.99, "availability": 0.8, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Coffee": {"price": 5.49, "availability": 0.6, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Tea": {"price": 3.99, "availability": 0.7, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Soda": {"price": 1.49, "availability": 0.9, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Bottled Water": {"price": 0.99, "availability": 1.0, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Chips": {"price": 2.79, "availability": 0.8, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Cookies": {"price": 3.49, "availability": 0.6, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Candy": {"price": 1.99, "availability": 0.7, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Ice Cream": {"price": 4.99, "availability": 0.5, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Frozen Pizza": {"price": 5.99, "availability": 0.5, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Canned Soup": {"price": 1.79, "availability": 0.9, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Honey": {"price": 4.29, "availability": 0.8, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Peanut Butter": {"price": 3.29, "availability": 0.7, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Jam": {"price": 2.99, "availability": 0.8, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Canned Vegetables": {"price": 1.69, "availability": 0.9, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Frozen Vegetables": {"price": 2.49, "availability": 0.8, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Olive Oil": {"price": 6.99, "availability": 0.6, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Vinegar": {"price": 2.19, "availability": 0.7, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Balsamic Vinegar": {"price": 4.99, "availability": 0.6, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Wine": {"price": 9.99, "availability": 0.4, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Beer": {"price": 1.99, "availability": 0.7, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Soy Sauce": {"price": 2.29, "availability": 0.8, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Worcestershire Sauce": {"price": 2.79, "availability": 0.6, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Pickles": {"price": 2.49, "availability": 0.8, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Olives": {"price": 3.29, "availability": 0.6, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Canned Tuna": {"price": 1.99, "availability": 0.7, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Canned Salmon": {"price": 3.99, "availability": 0.5, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Dried Fruits": {"price": 4.49, "availability": 0.6, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Nuts": {"price": 5.99, "availability": 0.5, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Hamburger Buns": {"price": 2.49, "availability": 0.9, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Hot Dog Buns": {"price": 2.29, "availability": 0.7, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Mustard": {"price": 1.79, "availability": 0.9, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Barbecue Sauce": {"price": 2.99, "availability": 0.8, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Salsa": {"price": 2.49, "availability": 0.7, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Tortilla Chips": {"price": 2.79, "availability": 0.8, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Sour Cream": {"price": 1.99, "availability": 0.9, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Cottage Cheese": {"price": 3.29, "availability": 0.6, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Pudding": {"price": 1.49, "availability": 0.95, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Cereal Bars": {"price": 3.49, "availability": 0.6, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Oatmeal": {"price": 2.99, "availability": 0.7, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Frozen Burritos": {"price": 4.49, "availability": 0.5, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Frozen Dinners": {"price": 3.99, "availability": 0.6, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Sausages": {"price": 5.49, "availability": 0.5, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Muffins": {"price": 2.79, "availability": 0.7, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Bagels": {"price": 2.49, "availability": 0.9, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Donuts": {"price": 1.99, "availability": 0.95, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Canned Fruit": {"price": 2.29, "availability": 0.9, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Fruit Cups": {"price": 2.99, "availability": 0.7, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Almond Milk": {"price": 3.49, "availability": 0.8, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Coconut Water": {"price": 2.79, "availability": 0.7, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Trail Mix": {"price": 4.9, "availability": 0.1, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
+    "Lemon Cake": {"price": 3.9, "availability": 0.6, 'type': 'E', 'delivery_takes_days': 7, 'goods_receipt_processing_days': 7}, # type(E) = 'in house production
 }
 
 # procurement
